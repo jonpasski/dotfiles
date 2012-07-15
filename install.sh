e4002083439073deb15d15476ddff3e352bd8ac7
Jon Passki <jon@passki.us>
2012-07-15 15:08:04 -0700

    Added screen love

--- install.sh
+++ install.sh
@@ -6,13 +6,17 @@ DOTFILES=${DOTFILES:-~/src/dotfiles};
 BACKUPS=${DOTFILES}/backups
 
 #TODO use test, very hackish for all of the below
-mkdir -p ${BACKUPS}/{vim,bash}
+mkdir -p ${BACKUPS}/{vim,bash,screen}
 
 cp ~/.vimrc ${BACKUPS}/vim/
 ln -fs ${DOTFILES}/vim/vimrc ~/.vimrc
 
 cp ~/.bash_login ${BACKUPS}/bash/
 ln -fs ${DOTFILES}/bash/bash_login ~/.bash_login
+
+# screen
+cp ~/.screenrc ${BACKUPS}/screen/
+ln -fs ${DOTFILES}/screen/screenrc ~/.screenrc
 
 # vundle
 mkdir -p ~/.vim/bundle
